5213597129a69b35f33ce79dcb6e4c339cc2cf70
Brenno Y. M. Luccas Correia <brenno.maluf@gmail.com>
2024-04-15 13:03:57 -0400

    Script - Ativar Itens do Orçamento

--- bikcraft/js/script.js
+++ bikcraft/js/script.js
@@ -1,12 +1,23 @@
+//Ativar links do Menu
 const links = document.querySelectorAll('.header-menu a');
 
 function ativarLink(link) {
   const url = location.href;
   const href = link.href;
-
   if (url.includes(href)) {
     link.classList.add('ativo');
   }
 }
-
 links.forEach(ativarLink)
+
+//Ativar Itens do Orçamento
+const parametros = new URLSearchParams(location.search)
+console.log(location)
+function ativarProduto(parametros) {
+  const elemento = document.getElementById(parametros)
+  if (elemento) {
+    elemento.checked = true
+  }
+}
+parametros.forEach(ativarProduto)
+
